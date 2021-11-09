e0084b28baaa8373880eefbf6cec249354b407b3
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-09 02:04:12 +0100

    creating includes folder

--- social_networking/views.py
+++ social_networking/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from accounts.models import UserProfile
 
 def home(request):
-    context = {}
+
+    user_profile = UserProfile.objects.all()
+    context = {
+        'user_profile': user_profile
+    }
     return render(request, 'home.html', context)
