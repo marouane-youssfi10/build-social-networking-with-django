737ef7e07f0f051527c09ce63112788b2aeb7586
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-11 00:35:31 +0100

    rename home to home_login and create home_not_login

--- social_networking/urls.py
+++ social_networking/urls.py
@@ -22,7 +22,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.home, name='home'),
+    path('', views.home, name='home_not_login'),
+
     path('accounts/', include('accounts.urls'))
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
