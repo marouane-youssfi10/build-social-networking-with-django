abb80226e396225f6cff9d3c9963d77b6f437126
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-11 01:18:42 +0100

    customize home_login page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from accounts.models import UserProfile
 # Create your views here.
 
 def index(request):
-    return render(request, 'home.html')
+
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    print('user_profile = ', user_profile)
+    context = {
+        'user_profile': user_profile
+    }
+    return render(request, 'home.html', context)
