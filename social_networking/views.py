98bc26d91109542de85640eff7d54472af1dcd90
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-13 13:58:47 +0100

    if user is authenticated send him to page index

--- social_networking/views.py
+++ social_networking/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.models import UserProfile
 
 def home(request):
     user_profile = UserProfile.objects.all()
 
+    if request.user.is_authenticated:
+        return redirect('index')
     context = {
         'user_profile': user_profile,
     }
