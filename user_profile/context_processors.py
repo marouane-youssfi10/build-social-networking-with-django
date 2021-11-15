c4f12da06f8313f83f37611f78e8af959b484532
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-15 15:21:28 +0100

    delete some div.job-info in home_login page

--- user_profile/context_processors.py
+++ user_profile/context_processors.py
@@ -2,8 +2,5 @@ from accounts.models import UserProfile, Account
 
 def display_photo_profile(request):
     photo_profile_user = UserProfile.objects.get(user=request.user)
-    print('--------- context_processors ---------------')
-    print('photo_profile = ', photo_profile_user)
-    print('--------- context_processors ---------------')
     return dict(photo_profile_user=photo_profile_user.photo_profile)
 
