9054cbe974f01257bbe86af24f839119eab0b8fa
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-16 00:41:14 +0100

    cosutomize update info title and content

--- user_profile/views.py
+++ user_profile/views.py
@@ -39,12 +39,12 @@ def user_profile(request):
 @login_required(login_url='login')
 def edit_profile(request):
 
-    print('\n---------- Piw Piw ----------\n')
     user_profile = get_object_or_404(UserProfile, user=request.user)
     print('\nuserprofile = ', user_profile, '\n')
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        print('\n', profile_form, '\n')
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
@@ -53,7 +53,7 @@ def edit_profile(request):
             context = {
                 'userprofile': user_profile,
             }
-            return render(request, 'edit_profile.html', context)
+            return render(request, 'profile_user/edit_profile.html', context)
     else:
         user_form = UserForm(instance=request.user)
         profile_form = UserProfileForm(instance=user_profile)
