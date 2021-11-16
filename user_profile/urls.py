f39bfa79f0d6205fde11be4dc7cc362811a5557e
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-16 13:17:38 +0100

    fixing edit_profile 3

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('user-profile/', views.user_profile, name="user-profile"),
     path('accounts-setting/change-password/', views.change_password, name="change-password"),
-    path('accounts-setting/edit-profile/<int:pk>/', views.edit_profile, name="edit-profile")
+    path('accounts-setting/edit-profile/', views.edit_profile, name="edit-profile")
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -3,6 +3,7 @@ from accounts.models import UserProfile, Experience_user
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm, UserProfileForm, ExperienceUserForm
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 @login_required(login_url='login')
@@ -37,46 +38,49 @@ def user_profile(request):
 
 
 @login_required(login_url='login')
-def edit_profile(request, pk):
+def edit_profile(request):
+    try:
+        user_profile = get_object_or_404(UserProfile, user=request.user)
+        user_experience = get_object_or_404(Experience_user, experience_user=request.user)
+        print('user_profile = ', user_profile, '--', user_profile.pk)
+        print('user_experience = ', user_experience, '--', user_experience.pk, '\n')
 
-    # get request user
-    user_profile = get_object_or_404(UserProfile, id=pk)
-    user_experience = get_object_or_404(Experience_user, id=pk)
-    print('\n user_profile = ', user_profile, '--', user_profile.id, '\n')
-    print('\n user_experience = ', user_experience, '--', user_experience.id, '\n')
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        experience_form = ExperienceUserForm(request.POST, request.FILES, instance=user_experience)
-        profile_form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        if request.method == 'POST':
+            user_form = UserForm(request.POST, instance=request.user)
+            experience_form = ExperienceUserForm(request.POST, request.FILES, instance=user_experience)
+            profile_form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
 
-        if user_form.is_valid() and profile_form.is_valid() and experience_form.is_valid():
-            print('\nbaby\n')
-            experience_form.experience_user_id = user_experience.id
-            profile_form.experience.user_experience = user_experience.id
+            if user_form.is_valid() and profile_form.is_valid() and experience_form.is_valid():
+                experience_form.experience_user_id = user_experience.id
+                profile_form.experience.user_experience = user_experience.id
 
-            user_form.save()
-            experience_form.save()
-            profile_form.save()
-            messages.success(request, 'Your profile has been updated.')
+                user_form.save()
+                experience_form.save()
+                profile_form.save()
+                messages.success(request, 'Your profile has been updated.')
 
-            context = {
-                'userprofile': user_profile,
-            }
-            return render(request, 'profile_user/edit_profile.html', context)
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = UserProfileForm(instance=user_profile)
-        experience_form = ExperienceUserForm(instance=user_experience)
+                context = {
+                    'userprofile': user_profile,
+                }
+                return render(request, 'profile_user/edit_profile.html', context)
+        else:
+            user_form = UserForm(instance=request.user)
+            profile_form = UserProfileForm(instance=user_profile)
+            experience_form = ExperienceUserForm(instance=user_experience)
 
-    # print('\n-------------- edit_profile --------------------\n')
-    context = {
-        'user_form': user_form,
-        'profile_form': profile_form,
-        'experience_form': experience_form,
-        'user_profile': user_profile
-    }
+        # print('\n-------------- edit_profile --------------------\n')
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'experience_form': experience_form,
+            'user_profile': user_profile
+        }
 
-    return render(request, 'profile_user/edit_profile.html', context)
+        return render(request, 'profile_user/edit_profile.html', context)
+
+    except ObjectDoesNotExist:
+        pass
+
 
 def change_password(request):
     user_profile = UserProfile.objects.get(user=request.user)
