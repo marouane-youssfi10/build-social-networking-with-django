7b33c9db4386dc7d1d0326e51da2ac061515ec5a
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-17 21:17:54 +0100

    fixing create tags

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 
     path('accounts-setting/change-password/', views.change_password, name="change-password"),
     path('accounts-setting/edit-profile/', views.edit_profile, name="edit-profile"),
-    path('accounts-setting/edit-profile/experience/', views.edit_experience_user, name="edit-user-experience"),
-    path('accounts-setting/edit-profile/tags/', views.edit_tags_user, name="edit-user-tags")
+    path('accounts-setting/edit-profile/edit_experience/', views.edit_experience_user, name="edit-user-experience"),
+    path('accounts-setting/edit-profile/create_tags/', views.create_tags_user, name="create-user-tags")
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from accounts.models import UserProfile, Experience_user, Tags
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm, UserProfileForm, ExperienceUserForm, TagsUserForm
@@ -11,17 +10,10 @@ from django.core.exceptions import ObjectDoesNotExist
 @login_required(login_url='login')
 def index(request):
     if request.user.is_authenticated:
-        print('\n-------------------- if --------------------')
         user_profile = UserProfile.objects.get(user=request.user)
         all_user_profile = UserProfile.objects.all()
     else:
-        print('\n------------------- else -------------------')
         return redirect('home')
-
-    print('\n-----------------------------------------------------------------------')
-    print('user_profile ', user_profile)
-    print('all_user_profile = ', all_user_profile)
-    print('\n-----------------------------------------------------------------------')
 
     context = {
         'user_profile': user_profile,
@@ -111,11 +103,43 @@ def edit_experience_user(request):
 
     except ObjectDoesNotExist:
         pass
-    return HttpResponse('Edit User')
+
 
 @login_required(login_url='login')
-def edit_tags_user(request):
-    pass
+def create_tags_user(request):
+    try:
+        # get user_tags & user_profile
+        user_profile = UserProfile.objects.get(user=request.user)
+        skills_tags = Tags.objects.filter(tags_user=request.user)
+        # user_profile_tags = user_tags.user_tags.all()
+        if request.method == 'POST':
+            # get information of usertags
+            user_tags_form = TagsUserForm(request.POST, request.FILES, instance=request.user)
+
+            print('\nuser_tags_form.is_valid() = ', user_tags_form.is_valid(), '\n')
+            # check user_form & profile_form is valid
+            if user_tags_form.is_valid():
+                # save the information updated
+                tag = Tags.objects.create(
+                    tags_user=request.user,
+                    tag=user_tags_form.cleaned_data['tag']
+                )
+                user_profile.skills_tags.add(request.user)
+                #  room.participants.add(request.user)
+                tag.save()
+                messages.success(request, 'Your profile has been updated.')
+                return redirect('/')
+        else:
+            user_tags_form = TagsUserForm(instance=request.user)
+
+        context = {
+            'user_tags_form': user_tags_form,
+            'user_tags': user_tags
+        }
+        return render(request, 'profile_user/edit_profile.html', context)
+
+    except ObjectDoesNotExist:
+        pass
 
 
 def change_password(request):
