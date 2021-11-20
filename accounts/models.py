5d5969cd0c908b02530f0eaa8239d1cce59aa2df
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-20 16:41:24 +0100

    add null true to skills_tags

--- accounts/models.py
+++ accounts/models.py
@@ -103,7 +103,7 @@ class UserProfile(models.Model):
     education_description = models.TextField(blank=True, null=True)
     location_country = models.CharField(blank=True, max_length=100)
     location_city = models.CharField(blank=True, max_length=100)
-    skills_tags = models.ManyToManyField(Tags, blank=True)
+    skills_tags = models.ManyToManyField(Tags, blank=True, null=True)
     hourly_work = models.IntegerField(blank=True, null=True)
     type_work = models.CharField(blank=True, choices=STATUS_CHOICES, max_length=50, null=True, default=None)
 
