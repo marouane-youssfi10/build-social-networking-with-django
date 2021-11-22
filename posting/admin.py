6979a958d7c7e29dfc63b6b195eb2dd634a0b0e4
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-22 14:18:27 +0100

    change price from charfield to integerfield

--- posting/admin.py
+++ posting/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import PostProject, TagsProjects
 # Register your models here.
+
+admin.site.register(PostProject)
+admin.site.register(TagsProjects)
--- posting/models.py
+++ posting/models.py
@@ -7,8 +7,8 @@ class TagsProjects(models.Model):
     tag = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
-        verbose_name = 'tagsProjects'
-        verbose_name_plural = 'TagsUserProjects'
+        verbose_name = 'tags Projects'
+        verbose_name_plural = 'TagsUsers Projects'
 
     def __str__(self):
         return self.tag if self.tag else ''
@@ -17,11 +17,15 @@ class PostProject(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name_project = models.CharField(max_length=100, blank=False, null=False)
-    start_price = models.CharField(max_length=100, blank=False, null=False)
-    end_price = models.CharField(max_length=100, blank=False, null=False)
+    start_price = models.IntegerField(blank=False, null=False)
+    end_price = models.IntegerField(blank=False, null=False)
     description_project = models.TextField(blank=False, null=False)
 
     skills_tags_projects = models.ManyToManyField(TagsProjects, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Post projects'
+        verbose_name_plural = 'Projects'
 
     def __str__(self):
         return self.name_project
