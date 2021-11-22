7667c5b867d4f96178bbd2823dfc0306c582ded2
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-11-22 14:27:21 +0100

    customize admin posting app

--- posting/admin.py
+++ posting/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import PostProject, TagsProjects
 # Register your models here.
 
-admin.site.register(PostProject)
-admin.site.register(TagsProjects)
+class TagsProjectsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tags_users_projects', 'tag')
+
+class PostProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'user_profile', 'name_project', 'start_price', 'end_price')
+
+admin.site.register(PostProject, PostProjectAdmin)
+admin.site.register(TagsProjects, TagsProjectsAdmin)
