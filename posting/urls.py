1b138a3c8a1f0cded1cd86bdb10aa9a96cb676c8
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-06 13:30:37 +0100

    change name urls filter to filter-projects and the same for jobs

--- posting/urls.py
+++ posting/urls.py
@@ -6,15 +6,15 @@ urlpatterns = [
     path('projects/', views.index, name="projects"),
     path('post-project/', views.post_projects, name="post-project"),
     path('projects/search/', views.search_projects, name="search-projects"),
-    path('projects/filter/', views.filter_project, name="filter"),
+    path('projects/filter/', views.filter_project, name="filter-projects"),
     path('projects/edit-post/<int:pk>/', views.edit_post_project, name="edit-post-project"),
     path('projects/delete-tag-post/<int:pk>/<int:project_post_id>/', views.delete_tag_post, name="delete-tag-post"),
     path('projects/create-tag-post/<int:project_post_id>/', views.create_tags_post, name="create-tags-post"),
 
     # page Jobs
-    path('jobs/', views.post_job, name="post-job"),
+    path('jobs/', views.jobs, name="post-job"),
     path('jobs/search/', views.search_jobs, name="search-jobs"),
-    path('jobs/filter/', views.filter_jobs, name="filter"),
+    path('jobs/filter/', views.filter_jobs, name="filter-jobs"),
 
 
 
--- posting/views.py
+++ posting/views.py
@@ -20,57 +20,7 @@ def index(request):
     }
     return render(request, 'pages/projects.html', context)
 
-def post_projects(request):
-    if request.method == 'POST':
-        form_post_project = PostProjectForm(request.POST)
-        form_tags_post_project = TagsProjectsForm(request.POST)
-
-        print('form_post_projects.is_valid()     = ', form_post_project.is_valid())
-        print('form_tags_post_project.is_valid() = ', form_tags_post_project.is_valid())
-        tags_objs = []
-        if form_post_project.is_valid() and form_tags_post_project.is_valid():
-            # get the values of post_project
-            name_project = form_post_project.cleaned_data['name_project']
-            type_work_project = form_post_project.cleaned_data['type_work_project']
-            location = form_post_project.cleaned_data['location']
-            start_price = form_post_project.cleaned_data['start_price']
-            end_price = form_post_project.cleaned_data['end_price']
-            description_project = form_post_project.cleaned_data['description_project']
-
-            # get the value of tags_post_values list
-            tags_projects = form_tags_post_project.cleaned_data['tag']
-            tags_list = list(tags_projects.split(',')) # separate values with commas
-            for tag in tags_list:
-                # save the information updated
-                tag, created = TagsProjects.objects.get_or_create(tags_users_projects=request.user, tag=tag)
-                tags_objs.append(tag)
-
-            form_post_projects = PostProject.objects.create(
-                user=request.user,
-                name_project=name_project,
-                type_work_project=type_work_project,
-                location=location,
-                start_price=start_price,
-                end_price=end_price,
-                description_project=description_project
-            )
-            # add tag_obj to skills tags projects
-            form_post_projects.skills_tags_projects.set(tags_objs)
-            form_post_projects.save()
-
-            messages.success(request, 'Your Project is created')
-            return redirect('/projects/')
-    else:
-        form_post_project = PostProjectForm()
-        form_tags_post_project = TagsProjectsForm()
-
-    context = {
-        'form_post_project': form_post_project,
-        'form_tags_post_project': form_tags_post_project
-    }
-    return render(request, 'post/post_project.html', context)
-
-def post_job(request):
+def jobs(request):
     all_user_profile = UserProfile.objects.all()
 
     context = {
@@ -128,6 +78,56 @@ def filter_jobs(request):
         'all_user_profile': all_user_profile
     }
     return render(request, 'pages/jobs.html', context)
+
+def post_projects(request):
+    if request.method == 'POST':
+        form_post_project = PostProjectForm(request.POST)
+        form_tags_post_project = TagsProjectsForm(request.POST)
+
+        print('form_post_projects.is_valid()     = ', form_post_project.is_valid())
+        print('form_tags_post_project.is_valid() = ', form_tags_post_project.is_valid())
+        tags_objs = []
+        if form_post_project.is_valid() and form_tags_post_project.is_valid():
+            # get the values of post_project
+            name_project = form_post_project.cleaned_data['name_project']
+            type_work_project = form_post_project.cleaned_data['type_work_project']
+            location = form_post_project.cleaned_data['location']
+            start_price = form_post_project.cleaned_data['start_price']
+            end_price = form_post_project.cleaned_data['end_price']
+            description_project = form_post_project.cleaned_data['description_project']
+
+            # get the value of tags_post_values list
+            tags_projects = form_tags_post_project.cleaned_data['tag']
+            tags_list = list(tags_projects.split(',')) # separate values with commas
+            for tag in tags_list:
+                # save the information updated
+                tag, created = TagsProjects.objects.get_or_create(tags_users_projects=request.user, tag=tag)
+                tags_objs.append(tag)
+
+            form_post_projects = PostProject.objects.create(
+                user=request.user,
+                name_project=name_project,
+                type_work_project=type_work_project,
+                location=location,
+                start_price=start_price,
+                end_price=end_price,
+                description_project=description_project
+            )
+            # add tag_obj to skills tags projects
+            form_post_projects.skills_tags_projects.set(tags_objs)
+            form_post_projects.save()
+
+            messages.success(request, 'Your Project is created')
+            return redirect('/projects/')
+    else:
+        form_post_project = PostProjectForm()
+        form_tags_post_project = TagsProjectsForm()
+
+    context = {
+        'form_post_project': form_post_project,
+        'form_tags_post_project': form_tags_post_project
+    }
+    return render(request, 'post/post_project.html', context)
 
 def search_projects(request):
     print('-------------search projects -------------------------')
