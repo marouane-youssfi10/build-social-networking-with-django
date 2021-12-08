cee78768e1fbf5abb70d88d039caa75d959b5630
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-08 23:51:03 +0100

    add script code to hide comment section

--- social_networking/static/js/script.js
+++ social_networking/static/js/script.js
@@ -60,6 +60,27 @@ $(window).on("load", function() {
         return false;
     });
 
+
+    //  ============= HIDE COMMENT SECTION =========
+       // A function that hides or shows a selected element
+       function hideOrShow() {
+
+          // Select the element with id "theDIV"
+          var x = document.getElementById("theDIV");
+
+          // If selected element is hidden
+          if (x.style.display === "none") {
+
+            // Show the hidden element
+            x.style.display = "block";
+
+            // Else if the selected element is shown
+          } else {
+
+            // Hide the element
+            x.style.display = "none";
+          }
+       }
     //  ============= COVER GAP FUNCTION =========
 
     var gap = $(".container").offset().left;
