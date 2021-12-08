ce54e7f38ba5e5fa4289095d24086523d87c2856
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-09 00:46:18 +0100

    add scroll to comment section

--- social_networking/static/js/script.js
+++ social_networking/static/js/script.js
@@ -63,24 +63,16 @@ $(window).on("load", function() {
 
     //  ============= HIDE COMMENT SECTION =========
        // A function that hides or shows a selected element
-       function hideOrShow() {
-
+       $('.com').on("click", function(){
           // Select the element with id "theDIV"
-          var x = document.getElementById("theDIV");
-
-          // If selected element is hidden
+          var x = document.getElementById("comment");
           if (x.style.display === "none") {
-
-            // Show the hidden element
             x.style.display = "block";
-
-            // Else if the selected element is shown
           } else {
-
-            // Hide the element
             x.style.display = "none";
           }
-       }
+       });
+
     //  ============= COVER GAP FUNCTION =========
 
     var gap = $(".container").offset().left;
