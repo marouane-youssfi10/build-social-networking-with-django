2bf1002c32da9c856f27662aedc07c6d0e5b0caa
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-09 01:19:12 +0100

    setting models comment app

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# models
+from accounts.models import Account
+from posting.models import PostProject
 
-# Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey(PostProject, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
