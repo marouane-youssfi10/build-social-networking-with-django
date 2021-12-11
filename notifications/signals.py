faa67dea516918252ab89e686bf94bbf35246dfb
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-11 20:25:21 +0100

    find a list(pk_set)[0] fot getting a value from pk_set

--- notifications/signals.py
+++ notifications/signals.py
@@ -3,38 +3,34 @@ from django.dispatch import receiver
 # models
 from posting.models import PostProject
 from .models import NotificationProjects
+from accounts.models import Account
 
 # @receiver(m2m_changed, sender=PostProject.likes.through)
 """@receiver(post_save, sender=PostProject)
 def send_like_notifications(sender, instance, created, **kwargs):
-    print('==============================================================')
-    print('sender                  = ', sender)
-    print('created                 = ', created)
-
-    # kana5do user dyal dik lpost
-    # post = user.post_project
-    post = instance
-    last_one = post.likes.all()
-    user = post.likes.last()
-    print('last_one                = ', last_one.last())
-    print('post                    = ', post.likes)
-    print('like.user               = ', post.user)
-    print('like.post_project.user  = ', user)
 
     # notify = NotificationProjects(post_project=post, sender=sender, to_user=post.user, notification_type=1)
     # notify.save()"""
 @receiver(m2m_changed, sender=PostProject.likes.through)
-def m2m_changed_likes(sender, instance, action, model, pk_set, **kwargs):
-    print('pk_set        = ', pk_set)
-    print('model         = ', model.user)
-    print('sender        = ', sender)
-    print('instance      = ', instance)
-    print('instance.user = ', instance.user)
-    print('instance.user = ', instance.likes.all())
-    print('action        = ', action)
-    print('------------------------------------')
-
+def m2m_changed_likes(sender, instance, action, pk_set, **kwargs):
+    if action == 'post_add' or action == 'post_remove':
+        print('pk_set                = ', pk_set)
+        print('sender                = ', sender)
+        print('instance              = ', instance)
+        print('instance.user         = ', instance.user)
+        print('instance.name_project = ', instance.name_project)
+        print('list(pk_set)[0] = ', list(pk_set)[0])
+        user_post = instance.user
+        request_user_id = pk_set
+        # request_user = Account.objects.get(id=request_user_id)
+        print('------------------------------------')
+        #                               PostProject             Account              Account
+        # notify = NotificationProjects(post_project=user_post, sender=request_user, to_user=user_post, notification_type=1)
+        # notify.save()
 
 """
 NOTIFICATION_TYPES  ,post_project ,sender  ,to_user ,notification_type ,text_preview ,created
+post_project : noti_post_project
+sender       : noti_project_from_user
+to_user      : noti_project_to_user
 """
