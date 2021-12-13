954a46a24067b1c9fae7f9f3bcfe433616bfb3c2
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-13 16:05:35 +0100

    fix notfication is_seen and do update o True

--- notifications/views.py
+++ notifications/views.py
@@ -3,12 +3,13 @@ from .models import NotificationProjects
 
 def show_notifications(request):
     notifications = NotificationProjects.objects.filter(to_user=request.user)
-    NotificationProjects.objects.filter(to_user=request.user, is_seen=False).update(is_seen=True)
 
-    print('\nnotifications = ', notifications, '\n')
     context = {
         'notifications': notifications
     }
-    return render(request, 'notifications/notifications.html', context)
+    response = render(request, 'notifications/notifications.html', context)
+    NotificationProjects.objects.filter(to_user=request.user, is_seen=False).update(is_seen=True)
+    return response
+
 
 
