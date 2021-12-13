ae491c5e0f711cdee09813b57a12482d7040d4ff
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-13 20:26:54 +0100

    delete NotificiationsJobs class from notifications app

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-from .models import NotificationJobs, NotificationProjects
+from .models import NotificationProjects
 
 class NotificationProjectsAdmin(admin.ModelAdmin):
     list_display = ['id', 'post_project', 'to_user', 'notification_type', 'sender', 'body', 'created', 'is_seen']
     list_editable = ['is_seen']
 
-class NotificationJobsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'post_job', 'to_user', 'notification_type', 'sender', 'body', 'created', 'is_seen']
-    list_editable = ['is_seen']
 
 admin.site.register(NotificationProjects, NotificationProjectsAdmin)
-admin.site.register(NotificationJobs, NotificationJobsAdmin)
--- notifications/models.py
+++ notifications/models.py
@@ -5,6 +5,7 @@ from posting.models import PostProject
 
 class NotificationProjects(models.Model):
     NOTIFICATION_TYPES = ((1, 'Like'), (2, 'Comment'), (3, 'Follow'))
+    post_job = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='noti_post_job', blank=True,null=True)
     post_project = models.ForeignKey(PostProject, on_delete=models.CASCADE, related_name='noti_post_project', blank=True, null=True)
     sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='noti_project_from_user')
     to_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='noti_project_to_user')
@@ -18,23 +19,6 @@ class NotificationProjects(models.Model):
 
     class Meta:
         verbose_name = 'Notification'
-        verbose_name_plural = 'Notification Projects'
+        verbose_name_plural = 'Notification Projects Jobs'
         ordering = ['-created']
 
-class NotificationJobs(models.Model):
-    NOTIFICATION_TYPES = ((1, 'Like'), (2, 'Comment'), (3, 'Follow'))
-    post_job = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='noti_post_job', blank=True, null=True)
-    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='noti_job_from_user')
-    to_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='noti_job_to_user')
-    notification_type = models.IntegerField(choices=NOTIFICATION_TYPES)
-    body = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    is_seen = models.BooleanField(default=False)
-
-    def __str__(self):
-        return str(self.sender)
-
-    class Meta:
-        verbose_name = 'Notification'
-        verbose_name_plural = 'Notification Jobs'
-        ordering = ['-created']
