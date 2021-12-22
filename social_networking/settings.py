2c8618624dd2e3e9b23aea707277f7089dc1220c
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-22 14:52:20 +0100

    customize setting

--- social_networking/settings.py
+++ social_networking/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "social_networking",
         'USER': 'postgres',
-        'PASSWORD': '****',
+        'PASSWORD': env('PASSWORD'),
         'HOST': 'localhost',
     }
 }
