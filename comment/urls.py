5e422358bfa54d820e5c543b82dcf15eda16c6a7
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-27 00:56:13 +0100

    change url path name

--- comment/urls.py
+++ comment/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('post/<int:project_id>', views.comment_project, name="comment"),
+    path('post-project/<int:project_id>', views.comment_project, name="comment"),
     path('comment-post-project/<int:project_id>', views.post_comment_project, name="post-comment-project"),
     path('delete-comment-project/<int:comment_id>', views.delete_comment_project, name='delete-comment'),
     path('edit-comment/<int:comment_id>', views.edit_comment_project, name='edit-comment'),
 
-    path('post-jobs/<int:jobs_id>', views.comment_jobs, name="comment-jobs"),
-    path('comment-post-jobs/<int:jobs_id>', views.post_comment_jobs, name="post-comment-jobs"),
-    path('delete-comment-jobs/<int:comment_id>', views.delete_comment_jobs, name='delete-comment-jobs'),
-    path('edit-comment-jobs/<int:comment_id>', views.edit_comment_jobs, name='edit-comment-jobs'),
-    ]
+    path('post-job/<int:jobs_id>', views.comment_jobs, name="comment-jobs"),
+    path('comment-post-job/<int:jobs_id>', views.post_comment_jobs, name="post-comment-jobs"),
+    path('delete-comment-job/<int:comment_id>', views.delete_comment_jobs, name='delete-comment-jobs'),
+    path('edit-comment-job/<int:comment_id>', views.edit_comment_jobs, name='edit-comment-jobs'),
+]
