f439cf09b2ba6e43c19837bc96d86d62b2e1becd
marouane youssfi <marouaneyoussfi5@gmail.com>
2021-12-28 15:42:14 +0100

    update

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -209,47 +209,6 @@ $(window).on("load", function() {
         e.stopPropagation();
     });
 
-    //  ============= PORTFOLIO SLIDER FUNCTION =========
-
-    $('.profiles-slider').slick({
-        slidesToShow: 3,
-        slck:true,
-        slidesToScroll: 1,
-        prevArrow:'<span class="slick-previous"></span>',
-        nextArrow:'<span class="slick-nexti"></span>',
-        autoplay: true,
-        dots: false,
-        autoplaySpeed: 2000,
-        responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-
     // ================ Alert Message ===============
 
     setTimeout(function(){
